04ab0f80ed169c3551c37323651c0abb7ce3938e
SamzDev <samz-dev@hotmail.com>
2020-12-27 20:27:09 +0800

    完成HTML parse模块的文件拆分

---
+++ Week 09/HTML-Parse/server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {  
+    console.log("request received");
+    console.log(request.headers);
+    response.setHeader('Content-Type', 'text/html');
+    response.setHeader('X-Foo', 'bar');
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end(
+`<html maaa=a >
+<head>
+    <style>
+body div #myid{
+    width:100px;
+    background-color: #ff5000;
+}
+body div img{
+    with:30px;
+    background-color: #ff1111;
+}
+    </style>
+</head>
+<body>
+    <div>
+        <img id="myid"/>
+        <img />
+    </div>
+</body>
+</html>`);
+});
+
+server.listen(9000);
