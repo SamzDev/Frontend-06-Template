be12441261510fd2a3cc2bcf9bf3a265b0017844
SamzDev <samz-dev@hotmail.com>
2021-01-03 22:07:46 +0800

    完成“根据浏览器属性进行排版”

---
+++ Week 10/Layout/server.js
@@ -0,0 +1,35 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {  
+    console.log("request received");
+    console.log(request.headers);
+    response.setHeader('Content-Type', 'text/html');
+    response.setHeader('X-Foo', 'bar');
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end(
+`<html maaa=a >
+<head>
+    <style>
+#container {
+    width:500px;
+    height:300px;
+    display:flex;
+}
+#container #myid {
+    width:200px;
+}
+#container .c1 {
+    flex:1;
+}
+    </style>
+</head>
+<body>
+    <div id="container">
+        <div id="myid"/>
+        <div class="c1" />
+    </div>
+</body>
+</html>`);
+});
+
+server.listen(9000);
