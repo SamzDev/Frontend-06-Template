6c88b0dd740c23102a7c93d98b0063257387a6d4
SamzDev <samz-dev@hotmail.com>
2021-02-04 22:14:17 +0800

    完成-->初步建立动画和时间线

--- Week 15/jsx/main.js
+++ Week 15/jsx/main.js
@@ -1,83 +1,6 @@
-import {Component, createElement} from "./framework.js"
-
-class Carousel extends Component {
-    constructor() {
-        super();
-        this.attributes = Object.create(null);
-    }
-    setAttribute(name, value) {
-        this.attributes[name] = value;
-    }
-    render() {
-        this.root = document.createElement("div");
-        this.root.classList.add("carousel");
-        for (const record of this.attributes.src) {
-            let child = document.createElement("div");
-            child.style.backgroundImage = `url('${record}')`;
-            this.root.appendChild(child);
-        }
-
-        let position = 0;
-        this.root.addEventListener("mousedown", event => {
-            
-            let children = this.root.children;
-            let startX = event.clientX;
-
-            let move = event => {
-                let x = event.clientX - startX;
-                let current = position - ((x - x % 500) / 500);
-                for (const offset of [-1, 0, 1]) {
-                    let pos = current + offset;
-                    pos = (pos + children.length) % children.length;
-                    children[pos].style.transition = "none";
-                    children[pos].style.transform = `translateX(${-pos * 500 + offset * 500 + x % 500}px)`;
-                }
-            }
-            let up = event => {
-                let x = event.clientX - startX;
-                position = position - Math.round(x / 500);
-                for (const offset of [0, - Math.sign(Math.round(x / 500) - x + 250 * Math.sign(x))]) {
-                    let pos = position + offset;
-                    pos = (pos + children.length) % children.length;
-                    children[pos].style.transition = "";
-                    children[pos].style.transform = `translateX(${-pos * 500 + offset * 500}px)`;
-                }
-                console.log("mouseup");
-                document.removeEventListener("mousemove",move);
-                document.removeEventListener("mouseup",up);
-            }
-            document.addEventListener("mousemove", move);
-            document.addEventListener("mouseup", up);
-        });
-        
-/*
-        let currentIndex = 0;
-        setInterval(() => {
-            let children = this.root.children;
-            let nextIndex = (currentIndex + 1) % children.length;
-            let current = children[currentIndex];
-            let next = children[nextIndex];
-            next.style.transition = "none";
-            next.style.transform = `translateX(${100 - nextIndex * 100}%)`;
-            
-            setTimeout(() => {
-                next.style.transition = "";
-                current.style.transform = `translateX(${-100 - currentIndex * 100}%)`;
-                next.style.transform = `translateX(${-nextIndex * 100}%)`
-                currentIndex = nextIndex;
-            },16);
-            
-        }, 3000);
-*/
-
-        return this.root;
-    }
-    mountTo(parent) {
-        console.log(this.attributes.src);
-        parent.appendChild(this.render());
-    }
-    
-}
+import {createElement} from "./framework.js"
+import {Carousel} from "./carousel.js"
+import {Timeline, Animation} from "./animation.js"
 
 let d = [
     "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
@@ -89,3 +12,10 @@ let d = [
 let a = <Carousel src={d}/>
 // document.body.appendChild(a);
 a.mountTo(document.body);
+
+let tl = new Timeline();
+
+tl.add(new Animation({set a(v) {
+    console.log(v)
+}}, "a", 0, 100, 1000, null));
+tl.start();
