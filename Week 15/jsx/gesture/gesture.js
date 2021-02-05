f33ef68c357b9be3998a57d11f7e3d99735d2c72
SamzDev <samz-dev@hotmail.com>
2021-02-06 01:37:22 +0800

    完成 -> 派发事件

--- Week 15/jsx/gesture/gesture.js
+++ Week 15/jsx/gesture/gesture.js
@@ -43,16 +43,16 @@ element.addEventListener("mousedown", event => {
         contexts.delete("mouse" + (1 << event.button));
 
         if (event.buttons === 0) {
-            element.removeEventListener("mousemove", mousemove);
-            element.removeEventListener("mouseup", mouseup);
+            document.removeEventListener("mousemove", mousemove);
+            document.removeEventListener("mouseup", mouseup);
             isListeningMouse = false;          
         }
         
     }
 
     if (!isListeningMouse) {
-        element.addEventListener("mousemove", mousemove);
-        element.addEventListener("mouseup", mouseup);
+        document.addEventListener("mousemove", mousemove);
+        document.addEventListener("mouseup", mouseup);
         isListeningMouse = true;
     }
     
@@ -126,7 +126,7 @@ let move = (point, context) => {
 
 let end = (point, context) => {
     if (context.isTap) {
-        console.log("tap");
+        dispatch("tap", {});
         clearTimeout(context.handler);
     }
     if (context.isPan) {
@@ -141,4 +141,12 @@ let end = (point, context) => {
 let cancel = (point, context) => {
     clearTimeout(context.handler);
     // console.log("cancel", point.clientX, point.clientY);
+}
+
+function dispatch(type, properties) {
+    let event = new Event(type);
+    for (const name in properties) {
+        event[name] = properties[name];
+    }
+    element.dispatchEvent(event);
 }
