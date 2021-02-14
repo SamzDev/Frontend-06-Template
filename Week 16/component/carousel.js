678a82369466736785858e1d4ebd7cca205bbde2
SamzDev <samz-dev@hotmail.com>
2021-02-14 10:36:47 +0800

    完成 -> 手势动画应用

--- Week 16/component/carousel.js
+++ Week 16/component/carousel.js
@@ -1,4 +1,7 @@
 import {Component} from "./framework.js"
+import {enableGesture} from "./gesture.js"
+import {Timeline, Animation} from "./animation.js"
+import {ease} from "./ease.js"
 
 export class Carousel extends Component {
     constructor() {
@@ -11,13 +14,90 @@ export class Carousel extends Component {
     render() {
         this.root = document.createElement("div");
         this.root.classList.add("carousel");
+        // let index = 0;
         for (const record of this.attributes.src) {
             let child = document.createElement("div");
             child.style.backgroundImage = `url('${record}')`;
+            // index++;
+            // child.textContent = index.toString();
             this.root.appendChild(child);
         }
 
+        enableGesture(this.root);
+        let timeline = new Timeline;
+        timeline.start();
+
+        let handler = null;
+
+        let children = this.root.children;
         let position = 0;
+
+        let t = 0;
+        let ax = 0; // 动画产生的位移x
+
+        this.root.addEventListener("start", event => {
+            timeline.pause();
+            clearInterval(handler);
+            let progress = (Date.now() - t) / 500;
+            ax = ease(progress) * 500 - 500;
+        })
+
+        this.root.addEventListener("pan", event => {
+            let x = event.clientX - event.startX - ax;
+            let current = position - ((x - x % 500) / 500);
+            for (const offset of [-1, 0, 1]) {
+                let pos = current + offset;
+                pos = (pos % children.length + children.length) % children.length;
+                children[pos].style.transition = "none";
+                children[pos].style.transform = `translateX(${-pos * 500 + offset * 500 + x % 500}px)`;
+            }
+        })
+
+        this.root.addEventListener("end", event => {
+
+            timeline.reset();
+            timeline.start();
+            handler = setInterval(nextPicture, 3000);
+
+            let x = event.clientX - event.startX - ax;
+            let current = position - ((x - x % 500) / 500);
+
+            let direction = Math.round((x % 500) / 500);
+
+            if (event.isFlick) {
+                console.log(event.velocity);
+                if (event.velocity < 0) {
+                    direction = Math.ceil((x % 500) / 500);
+                } else {
+                    direction = Math.floor((x % 500) / 500);
+                }
+            }
+
+            for (const offset of [-1, 0, 1]) {
+                let pos = current + offset;
+                pos = (pos % children.length + children.length) % children.length;
+                
+                children[pos].style.transition = "none";
+                timeline.add(new Animation(children[pos].style, "transform", - pos * 500 + offset * 500 + x % 500, - pos * 500 + offset * 500 + direction * 500, 500, 0, ease, v => `translateX(${v}px)`));
+            }
+            
+            position = position - ((x - x % 500) / 500) - direction;
+            position = (position % children.length + children.length) % children.length;
+        })
+
+        let nextPicture = () => {
+            let children = this.root.children;
+            let nextIndex = (position + 1) % children.length;
+            let current = children[position];
+            let next = children[nextIndex];
+            t = Date.now();
+            timeline.add(new Animation(current.style, "transform", - position * 500, -500 - position * 500, 500, 0, ease, v => `translateX(${v}px)`));
+            timeline.add(new Animation(next.style, "transform", 500 - nextIndex * 500, - nextIndex * 500, 500, 0, ease, v => `translateX(${v}px)`));
+
+            position = nextIndex;
+            
+        }
+        handler = setInterval(nextPicture, 3000);
 
         /*
         this.root.addEventListener("mousedown", event => {
--- Week 16/component/gesture.js
+++ Week 16/component/gesture.js
@@ -114,6 +114,10 @@ export class Recognizer {
     start(point, context) {
         
         context.startX = point.clientX, context.startY = point.clientY;
+        this.dispatcher.dispatch("start", {
+            clientX: point.clientX,
+            clientY: point.clientY
+        })
         context.points = [{
             t: Date.now(),
             x: point.clientX,
@@ -212,9 +216,19 @@ export class Recognizer {
                 clientX: point.clientX,
                 clientY: point.clientY,
                 isVertical: context.isVertical,
-                isFlick: context.isFlick
+                isFlick: context.isFlick,
+                velocity: v
             });
         }
+        this.dispatcher.dispatch("end", {
+            startX: context.startX,
+            startY: context.startY,
+            clientX: point.clientX,
+            clientY: point.clientY,
+            isVertical: context.isVertical,
+            isFlick: context.isFlick,
+            velocity: v
+        });
     }
     
     cancel(point, context) {
