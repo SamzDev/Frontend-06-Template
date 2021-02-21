111536ca18f1e118fc760a0aa0aee74c72187777
SamzDev <samz-dev@hotmail.com>
2021-02-21 18:10:56 +0800

    完成 --> 初始化工具Yeoman（二）

--- Week 17/toolchain/generators/app/index.js
+++ Week 17/toolchain/generators/app/index.js
@@ -7,7 +7,23 @@ module.exports = class extends Generator {
         super(args, opts);
 
     }
-    
+
+    initPackage() {
+
+        const pkgJson = {
+            devDependencies: {
+              eslint: '^3.15.0'
+            },
+            dependencies: {
+              react: '^16.2.0'
+            }
+        };
+      
+        // Extend or create package.json file in destination path
+        this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
+        this.npmInstall();     
+    }
+
     async step1() {
         this.fs.copyTpl(
             this.templatePath('t.html'),
