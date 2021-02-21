97f7067a10db8337a2690101af188ffa18ceb6cd
SamzDev <samz-dev@hotmail.com>
2021-02-21 17:19:04 +0800

    完成 --> this.prompt功能实验

--- Week 17/toolchain/generators/app/index.js
+++ Week 17/toolchain/generators/app/index.js
@@ -8,11 +8,23 @@ module.exports = class extends Generator {
 
     }
     
-    method1() {
-        this.log('method 1 just ran');
+    async method1() {
+        const answers = await this.prompt([
+            {
+              type: "input",
+              name: "name",
+              message: "Your project name",
+              default: this.appname // Default to current folder name
+            },
+            {
+              type: "confirm",
+              name: "cool",
+              message: "Would you like to enable the Cool feature?"
+            }
+          ]);
+      
+        this.log("app name", answers.name);
+        this.log("cool feature", answers.cool);
     }
     
-    method2() {
-        this.log('method 2 just ran');
-    }
 };
