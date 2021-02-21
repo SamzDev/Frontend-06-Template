378a704ab85f0c88a243b62aa8769a0392687e9e
SamzDev <samz-dev@hotmail.com>
2021-02-21 17:54:36 +0800

    完成 --> file-system功能实验(templates)

--- Week 17/toolchain/generators/app/index.js
+++ Week 17/toolchain/generators/app/index.js
@@ -8,23 +8,12 @@ module.exports = class extends Generator {
 
     }
     
-    async method1() {
-        const answers = await this.prompt([
-            {
-              type: "input",
-              name: "name",
-              message: "Your project name",
-              default: this.appname // Default to current folder name
-            },
-            {
-              type: "confirm",
-              name: "cool",
-              message: "Would you like to enable the Cool feature?"
-            }
-          ]);
-      
-        this.log("app name", answers.name);
-        this.log("cool feature", answers.cool);
+    async step1() {
+        this.fs.copyTpl(
+            this.templatePath('t.html'),
+            this.destinationPath('public/index.html'),
+            { title: 'Templating with Yeoman' }
+        );        
     }
     
 };
