0f172812eec36695c242cfb8b94c235af2ce6008
SamzDev <samz-dev@hotmail.com>
2021-03-09 10:53:55 +0800

    完成 --> Node.js流的练习

--- Week 19/publish-server/server.js
+++ Week 19/publish-server/server.js
@@ -1,6 +1,11 @@
 let http = require('http');
 
-http.createServer(function (req, res) {
-    console.log(req);
-    res.end("Hello world from publish server");
+http.createServer(function (request, response) {
+    console.log(request.headers);
+    request.on('data', chunk => {
+        console.log(chunk.toString());
+    });
+    request.on('end', chunk => {
+        response.end("publish server --> success");
+    });
 }).listen(3000);
