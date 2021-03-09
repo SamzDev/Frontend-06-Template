eba68e51d5a5f9dc068b886600b5bf4239e78db2
SamzDev <samz-dev@hotmail.com>
2021-03-09 23:22:33 +0800

    完成 --> 实现多文件发布

--- Week 19/publish-server/server.js
+++ Week 19/publish-server/server.js
@@ -1,16 +1,11 @@
 let http = require("http");
-let fs = require("fs");
+let unzipper = require("unzipper");
 
 http.createServer(function (request, response) {
-    console.log(request.headers);
 
-    let outFile = fs.createWriteStream("../server/public/index.html");
+    console.log("request");
+    // let outFile = fs.createWriteStream("../server/public/tmp.zip");
+    // request.pipe(outFile);
+    request.pipe(unzipper.Extract({ path: '../server/public/' }));
 
-    request.on('data', chunk => {
-        outFile.write(chunk);
-    });
-    request.on('end', () => {
-        outFile.end();
-        response.end("publish server --> success");
-    });
 }).listen(8082);
--- Week 19/publish-tool/publish.js
+++ Week 19/publish-tool/publish.js
@@ -1,24 +1,27 @@
 let http = require("http");
 let fs = require("fs");
+let archiver = require("archiver");
 
-let file = fs.createReadStream("./sample.html");
 
 let request = http.request({
     hostname: "127.0.0.1",
     port: 8082,
     method: "POST",
     headers: {
-        'Content-Type': 'application/octet-stream'
+        'Content-Type': 'application/octet-stream',
+        // 'Content-Length': stats.size
     }
 }, response => {
     console.log(response);
 });
 
-file.on('data', chunk => {
-    console.log(chunk.toString());
-    request.write(chunk);
+// let file = fs.createReadStream("./sample.html");
+
+const archive = archiver('zip', {
+    zlib: { level: 9 } // Sets the compression level.
 });
-file.on('end', chunk => {
-    console.log("read finished");
-    request.end(chunk);
-})
+archive.directory('./sample/', false);
+
+archive.finalize();
+
+archive.pipe(request);
