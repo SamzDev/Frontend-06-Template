c20824307980512ce5611b613a4ee3db51e61a5e
SamzDev <samz-dev@hotmail.com>
2021-03-09 21:24:01 +0800

    完成 --> 改造server

--- Week 19/publish-server/server.js
+++ Week 19/publish-server/server.js
@@ -1,11 +1,16 @@
-let http = require('http');
+let http = require("http");
+let fs = require("fs");
 
 http.createServer(function (request, response) {
     console.log(request.headers);
+
+    let outFile = fs.createWriteStream("../server/public/index.html");
+
     request.on('data', chunk => {
-        console.log(chunk.toString());
+        outFile.write(chunk);
     });
-    request.on('end', chunk => {
+    request.on('end', () => {
+        outFile.end();
         response.end("publish server --> success");
     });
-}).listen(3000);
+}).listen(8082);
--- Week 19/publish-tool/publish.js
+++ Week 19/publish-tool/publish.js
@@ -1,11 +1,11 @@
 let http = require("http");
 let fs = require("fs");
 
-let file = fs.createReadStream("./package.json");
+let file = fs.createReadStream("./sample.html");
 
 let request = http.request({
     hostname: "127.0.0.1",
-    port: 3000,
+    port: 8082,
     method: "POST",
     headers: {
         'Content-Type': 'application/octet-stream'
