2fbf4ffd4e31666a448b5edf0481412eab50cb0d
SamzDev <samz-dev@hotmail.com>
2021-03-11 10:50:44 +0800

    完成 --> 用GitHub oAuth做一个登录实例

--- Week 19/publish-server/server.js
+++ Week 19/publish-server/server.js
@@ -1,11 +1,97 @@
 let http = require("http");
+let https = require("https");
 let unzipper = require("unzipper");
+let querystring = require('querystring');
+
+
+// 2. auth路由：接收code,用code+client_id+client_secret换token
+
+function auth(request, response) {
+    let query = querystring.parse(request.url.match(/^\/auth\?([\s\S]+)$/)[1]);
+    getToken(query.code, function (info) {
+        console.log(info);
+        // response.write(JSON.stringify(info));
+        response.write(`<a href='http://localhost:8083/?token=${info.access_token}'>publish</a>`)
+        response.end();
+    });
+}
+
+function getToken(code, callback) {
+    const client_id = "Iv1.7612dcbeb679b24d";
+    const client_secret = "86f44b8ce61f95ea8fe1a105dbf06dd5a32c1b82";
+
+    let request = https.request({
+        hostname: "github.com",
+        path: `/login/oauth/access_token?code=${code}&client_id=${client_id}&client_secret=${client_secret}`,
+        port: 443,
+        method: "POST"
+    }, function (response) {
+        let body = "";
+        response.on('data', chunk => {
+            body += (chunk.toString());
+        })
+        response.on('end', chunk => {
+            callback(querystring.parse(body));
+        })
+        // console.log(response);
+    });
+    request.end();
+}
+
+// 4. publish路由：用token获取用户信息，检查权限，接受发布
+
+function publish(request, response) {
+    let query = querystring.parse(request.url.match(/^\/publish\?([\s\S]+)$/)[1]);
+    if (query.token.length > 0) {
+        getUser(query.token, info => {
+            if (info.login === "SamzDev") {
+                request.pipe(unzipper.Extract({ path: '../server/public/' }));
+                request.on('end', function () {
+                    response.end("success !");
+                })  
+            }
+        });
+    }
+}
+
+function getUser(token, callback) {
+
+    let request = https.request({
+        hostname: "api.github.com",
+        path: `/user`,
+        port: 443,
+        method: "GET",
+        headers: {
+            Authorization: `token ${token}`,
+            "User-Agent": 'toy-publish-by-samdev'
+            // Accept: "application/vnd.github.v3+json"
+        }
+    }, function (response) {
+        let body = "";
+        response.on('data', chunk => {
+            body += (chunk.toString());
+        })
+        response.on('end', chunk => {
+            callback(JSON.parse(body));
+            // console.log(body);
+        })
+        
+    });
+    request.end();
+}
 
 http.createServer(function (request, response) {
 
-    console.log("request");
+    if (request.url.match(/^\/auth\?/)) {
+        return auth(request, response);
+    }
+    if (request.url.match(/^\/publish\?/)) {
+        return publish(request, response);
+    }
+
+    // console.log("request");
     // let outFile = fs.createWriteStream("../server/public/tmp.zip");
     // request.pipe(outFile);
-    request.pipe(unzipper.Extract({ path: '../server/public/' }));
+    // request.pipe(unzipper.Extract({ path: '../server/public/' }));
 
 }).listen(8082);
--- Week 19/publish-tool/publish.js
+++ Week 19/publish-tool/publish.js
@@ -1,27 +1,40 @@
 let http = require("http");
 let fs = require("fs");
 let archiver = require("archiver");
+let child_process = require("child_process");
+let querystring = require("querystring");
 
+// 1. 打开 https://github.com/login/oauth/authorize
 
-let request = http.request({
-    hostname: "127.0.0.1",
-    port: 8082,
-    method: "POST",
-    headers: {
-        'Content-Type': 'application/octet-stream',
-        // 'Content-Length': stats.size
-    }
-}, response => {
-    console.log(response);
-});
+child_process.exec(`open https://github.com/login/oauth/authorize?client_id=Iv1.7612dcbeb679b24d`);
 
-// let file = fs.createReadStream("./sample.html");
+// 3. 创建server，接受token，后点击发布
+http.createServer(function (request, response){
+    let query = querystring.parse(request.url.match(/^\/\?([\s\S]+)$/)[1]);
+    pubulish(query.token);
+    // response.end("token success!");
+    
+}).listen(8083);
 
-const archive = archiver('zip', {
-    zlib: { level: 9 } // Sets the compression level.
-});
-archive.directory('./sample/', false);
-
-archive.finalize();
-
-archive.pipe(request);
+function pubulish(token) {
+    let request = http.request({
+        hostname: "127.0.0.1",
+        port: 8082,
+        method: "POST",
+        path: "/publish?token=" + token,
+        headers: {
+            'Content-Type': 'application/octet-stream',
+        }
+    }, response => {
+        console.log(response);
+    });
+    
+    const archive = archiver('zip', {
+        zlib: { level: 9 } // Sets the compression level.
+    });
+    archive.directory('./sample/', false);
+    
+    archive.finalize();
+    
+    archive.pipe(request);
+}
